e479d3ed3a907b672ff5d66c6fe287a43b4f6239
Filip Vondrak <filip.vondrak4@seznam.cz>
2025-09-02 17:57:27 +0200

    Initial

---
+++ WebMovieDatabase/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebMovieDatabase.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
