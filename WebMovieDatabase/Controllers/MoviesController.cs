ba7fdd9802242f49bda8260a863fca4862e21048
Filip Vondrak <filip.vondrak4@seznam.cz>
2025-09-02 21:58:29 +0200

    Feat: added a AddMovie page and styled index page

--- WebMovieDatabase/Controllers/MoviesController.cs
+++ WebMovieDatabase/Controllers/MoviesController.cs
@@ -1,12 +1,68 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebMovieDatabase.Data;
+using WebMovieDatabase.Models;
 
 namespace WebMovieDatabase.Controllers;
 
+
 public class MoviesController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+
+    public MoviesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [Authorize]
+    public IActionResult AddMovie()
     {
         return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ValidateAntiForgeryToken]
+    public IActionResult AddMovie(Movie movie)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(movie);
+    }
+
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+        return View(movie);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id)
+    {
+        var movie = _context.Movies.Find(id);
+        if (movie != null)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Index()
+    {
+        var movies = _context.Movies.ToList();
+        return View(movies);
     }
 }
